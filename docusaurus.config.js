ae48e204665f1ca462372593a312a54d22a29fcf
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2023-08-27 00:39:29 +0300

    here we go

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -4,8 +4,8 @@
 const lightCodeTheme = require("prism-react-renderer/themes/github");
 const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
-const organizationName = "LayZeeDK";
-const projectName = "github-pages-docusaurus";
+const organizationName = "dodoGames-s-Studios";
+const projectName = "aoi.js-docs-by-dodo-v2";
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
